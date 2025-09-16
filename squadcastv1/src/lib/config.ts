153c0eea77452b2857a69cbe171fef1bb89ae787
Gaurav Bhatt <Gaurav.Bhatt@solarwinds.com>
2025-09-16 13:41:46 +0530

    chore: update ts sdk with studio

--- squadcastv1/src/lib/config.ts
+++ squadcastv1/src/lib/config.ts
@@ -61,7 +61,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.1.1",
+  sdkVersion: "0.1.2",
   genVersion: "2.698.4",
-  userAgent: "speakeasy-sdk/typescript 0.1.1 2.698.4 1.0.0 SquadcastSDK",
+  userAgent: "speakeasy-sdk/typescript 0.1.2 2.698.4 1.0.0 SquadcastSDK",
 } as const;
