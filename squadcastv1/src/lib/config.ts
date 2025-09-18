3c2129b561c2bc58e6a2488a4b4f8fd38acde8fc
bhattu-gauravv <gaurav.bhatt@solarwinds.com>
2025-09-18 11:45:33 +0530

    chore: sdk patch update

--- squadcastv1/src/lib/config.ts
+++ squadcastv1/src/lib/config.ts
@@ -61,7 +61,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.1.2",
-  genVersion: "2.698.4",
-  userAgent: "speakeasy-sdk/typescript 0.1.2 2.698.4 1.0.0 SquadcastSDK",
+  sdkVersion: "0.1.3",
+  genVersion: "2.702.0",
+  userAgent: "speakeasy-sdk/typescript 0.1.3 2.702.0 1.0.0 SquadcastSDK",
 } as const;
